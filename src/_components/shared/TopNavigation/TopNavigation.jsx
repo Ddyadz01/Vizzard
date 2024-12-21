c2df65d355157142b99b716b5b051b084b06cfd0
Ddyadz <albertmik2002@gmail.com>
2024-12-21 14:11:57 +0300

    FIx

--- src/_components/shared/TopNavigation/TopNavigation.jsx
+++ src/_components/shared/TopNavigation/TopNavigation.jsx
@@ -1,18 +1,23 @@
 import { AudioLines, CircleChevronLeft, CircleChevronRight, Search } from 'lucide-react';
 import NotificationIcon from '/public/icons/notification.svg?react';
-import ChartIcon from '/public/icons/chart.svg?react';
 import style from './top-navigation.module.scss';
+import { useAudioStore } from './../../../store/store';
 
 const TopNavigation = ({ isDashboard, setDashboard, setPlayerShow, isPlayerShow }) => {
+  const { activeAudio } = useAudioStore((state) => state);
   return (
     <div className={style['top-navigation']}>
       <div className={style['top-navigation--left']}>
         <div className={style['menu--btn']} onClick={() => setDashboard((prev) => !prev)}>
           {isDashboard ? <CircleChevronLeft /> : <CircleChevronRight />}
         </div>
-        <div className={style['player--toggle']} onClick={() => setPlayerShow((prev) => !prev)}>
-          {isPlayerShow ? <AudioLines stroke="var(--green-color)" /> : <AudioLines />}
-        </div>
+        {activeAudio?.audioUrl ? (
+          <div className={style['player--toggle']} onClick={() => setPlayerShow((prev) => !prev)}>
+            {isPlayerShow ? <AudioLines stroke="var(--green-color)" /> : <AudioLines />}
+          </div>
+        ) : (
+          ''
+        )}
       </div>
       <div className={style['top-navigation--right']}>
         <div className={style['search']}>
